d5ec8855a2738c7723398ee806dca73b9890ed25
Utngy Pisal <pisal.utngy@wbfinance.com.kh>
2022-09-16 14:03:43 +0700

    Hide some fields in menus

--- app/Models/MbTransactionChannel.php
+++ app/Models/MbTransactionChannel.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class MbTransactionChannel extends Model
 {
     use HasFactory;
+
+    protected $hidden = ["type", "parent_id", "created_at", "updated_at", "history"];
+
 }
